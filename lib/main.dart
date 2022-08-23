d17456a6240b45ad3aeab5f3561bed03397ed239
Joao Ricardi <joaoricardi@MacBook-Pro-de-Joao.local>
2022-08-23 17:50:43 -0300

    registrando o item do di para inicializar firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_challenge/src/app.dart';
+import 'package:flutter_challenge/src/core/di/di_handler.dart';
+import 'package:flutter_challenge/src/core/di/di_handler_imp.dart';
+import 'package:flutter_challenge/src/core/firebase/firebase_handler.dart';
 
-void main() {
+ main() async {
+
+  ///registrar classes na injecao de dependencia
+  IDIHandler _injection = DIHandlerImp()
+    ..init();
+
+  await _injection.get<IFirebaseHandler>().initFirebase();
+
   runApp(const MyApp());
 }
 
